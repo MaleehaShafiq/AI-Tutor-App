3dea780c275fbc4865ccbeba0d08504371d34f9f
Maliha Shafique <maleehashafiq2001@outlook.com>
2026-01-11 16:24:16 +0500

    Update tutor_app.py

--- tutor_app.py
+++ tutor_app.py
@@ -9,7 +9,10 @@ from pydantic import BaseModel, Field
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
+from langchain_core.output_parsers import (
+    StrOutputParser,
+    PydanticOutputParser,
+
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 from pydantic import BaseModel, Field
@@ -265,4 +268,5 @@ if st.session_state.stage == 'plan_display':
     except Exception as e:
         st.error(f"An error occurred during the planning stage. Please try again. Error: {e}")
 # =================================================================================================================================================================
+
 
